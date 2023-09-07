615fe45f1d4b53bcd9ba3b4eb39400d431991896
Seolin <bisor0627@gmail.com>
2023-09-07 16:41:30 +0900

    ✨ Add. TransformedWidget cardRect 외각 컬러 추가

--- image_editor/lib/ui/transformed_widget.dart
+++ image_editor/lib/ui/transformed_widget.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
-import '../utils/global.rect.dart';
-import 'clipper/center_clipper.dart';
+import 'package:image_editor/lib.dart';
 
 class TransformedWidget extends StatefulWidget {
-  final ThemeData? themeData;
+  final ThemeData themeData;
   final Widget? top;
   final Widget center;
   final Widget bottom;
@@ -12,7 +10,7 @@ class TransformedWidget extends StatefulWidget {
   final bool resizeToAvoidBottomInset;
   const TransformedWidget({
     super.key,
-    this.themeData,
+    required this.themeData,
     this.top,
     required this.center,
     required this.bottom,
@@ -32,47 +30,79 @@ class _TransformedWidgetState extends State<TransformedWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return widget.themeData != null
-        ? Theme(
-            data: widget.themeData!,
-            child: buildBody(context),
-          )
-        : buildBody(context);
+    return Theme(
+      data: widget.themeData,
+      child: buildBody(context),
+    );
   }
 
   Widget buildBody(BuildContext context) {
-    Widget body = ClipPath(
-      clipper: CenterWidthClip(width: GlobalRect().objectRect.width),
+    Widget body = CustomPaint(
+      painter: RectBlurPainter(GlobalRect().cardRect, MediaQuery.sizeOf(context), widget.themeData.canvasColor),
       child: Scaffold(
         resizeToAvoidBottomInset: widget.resizeToAvoidBottomInset,
-        body: Stack(
-          children: [
-            Transform.translate(
-              offset: Offset(0, GlobalRect().cardRect.top),
-              child: Center(
-                child: Column(
-                  children: [
-                    widget.center,
-                    SizedBox(
-                      height: GlobalRect().objectRect.top - GlobalRect().cardRect.bottom,
-                    ),
-                    widget.bottom,
-                  ],
+        body: ClipPath(
+          clipper: CenterWidthClip(width: GlobalRect().objectRect.width),
+          child: Stack(
+            children: [
+              Transform.translate(
+                offset: Offset(0, GlobalRect().cardRect.top),
+                child: Center(
+                  child: Column(
+                    children: [
+                      widget.center,
+                      SizedBox(
+                        height: GlobalRect().objectRect.top - GlobalRect().cardRect.bottom,
+                      ),
+                      widget.bottom,
+                    ],
+                  ),
                 ),
               ),
-            ),
-            if (widget.top != null)
-              Positioned(
-                top: GlobalRect().toolBarRect.top,
-                left: GlobalRect().toolBarRect.left,
-                child: widget.top!,
-              ),
-            if (widget.left != null) widget.left!,
-          ],
+              if (widget.top != null)
+                Positioned(
+                  top: GlobalRect().toolBarRect.top,
+                  left: GlobalRect().toolBarRect.left,
+                  child: widget.top!,
+                ),
+              if (widget.left != null) widget.left!,
+            ],
+          ),
         ),
       ),
     );
 
     return body;
+  }
+}
+
+class RectBlurPainter extends CustomPainter {
+  Rect? rect;
+  Size? viewSize;
+  final Color color;
+  Paint fillPaint = Paint();
+
+  RectBlurPainter(this.rect, this.viewSize, this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (rect == null || viewSize == null) return;
+
+    fillPaint.color = color;
+
+    // 전체 화면의 경로 생성
+    Path screenPath = Path()..addRect(Offset.zero & viewSize!);
+    // 주어진 rect의 경로 생성
+    Path rectPath = Path()..addRect(rect!);
+    // 화면 전체에서 rect 부분을 제외한 경로 생성
+    Path differencePath = Path.combine(PathOperation.difference, screenPath, rectPath);
+
+    // 여집합 부분에만 색상 칠하기
+    canvas.drawPath(differencePath, fillPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true; // 여기서는 무조건 다시 그리도록 설정했습니다. 최적화가 필요한 경우 조건을 수정하세요.
   }
 }
