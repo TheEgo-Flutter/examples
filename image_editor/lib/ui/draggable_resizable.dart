a5b3b3aae4e5462ffc0b0b63dfc640dc4909f221
Seolin <bisor0627@gmail.com>
2023-09-08 14:02:54 +0900

    ♻️ Refactor. DraggableResizable

--- image_editor/lib/ui/draggable_resizable.dart
+++ image_editor/lib/ui/draggable_resizable.dart
@@ -29,16 +29,42 @@ class DraggableResizable extends StatefulWidget {
   State<DraggableResizable> createState() => _DraggableResizableState();
 }
 
-class _DraggableResizableState extends State<DraggableResizable> {
+class _DraggableResizableState extends State<DraggableResizable> with SingleTickerProviderStateMixin {
+  Offset get offset => _offset;
   Offset _offset = Offset.zero;
+  set offset(Offset value) {
+    _offset = value;
+    setState(() {});
+  }
+
+  Size get size => _size;
   Size _size = Size.zero;
+  set size(Size value) {
+    _size = value;
+    setState(() {});
+  }
+
+  double get angle => _angle;
   double _angle = 0;
+  set angle(double value) {
+    _angle = value;
+    setState(() {});
+  }
+
+  dynamic get object => _object;
   dynamic _object;
-  double get _scale => (_size.width / widget.layerItem.rect.size.width);
+  set object(dynamic value) {
+    _object = value;
+    setState(() {});
+  }
+
+  Rect get rect => offset & size;
+  double get scale => (size.width / widget.layerItem.rect.size.width);
+
   LayerItem get layerItem => widget.layerItem.copyWith(
-        rect: _offset & _size,
-        angle: _angle,
-        object: _object,
+        rect: offset & size,
+        angle: angle,
+        object: object,
       );
 
   bool isCenteredHorizontally = false;
@@ -46,18 +72,24 @@ class _DraggableResizableState extends State<DraggableResizable> {
 
   Offset startingFingerPositionFromObject = Offset.zero;
   Offset currentFingerPosition = Offset.zero;
+
+  bool isInDeleteArea = false;
+
   @override
   void initState() {
     super.initState();
     final item = widget.layerItem;
     final aspectRatio = item.rect.size.width / item.rect.size.height;
-    _angle = widget.layerItem.angle;
-    _object = widget.layerItem.object;
-    _size = Size(item.rect.size.width, item.rect.size.width / aspectRatio);
-    _offset = item.rect.topLeft;
+    angle = widget.layerItem.angle;
+    object = widget.layerItem.object;
+    size = Size(item.rect.size.width, item.rect.size.width / aspectRatio);
+    offset = item.rect.topLeft;
   }
 
-  bool isInDeleteArea = false;
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   void _handleDeleteAction(
     bool isDragging,
@@ -65,10 +97,12 @@ class _DraggableResizableState extends State<DraggableResizable> {
     if (!(selectedLayerItem?.isObject ?? false)) return;
     if (!CardRect().deleteRect.contains(currentFingerPosition)) {
       isInDeleteArea = false;
+
       return;
     } else {
       if (isDragging) {
         if (!isInDeleteArea) HapticFeedback.lightImpact();
+
         isInDeleteArea = true;
       } else {
         widget.onDelete?.call(layerItem);
@@ -80,75 +114,46 @@ class _DraggableResizableState extends State<DraggableResizable> {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        if (widget.isFocus) ..._buildCenterLine(isCenteredHorizontally, isCenteredVertically),
+        if (widget.isFocus) ..._buildCenterLine(GlobalRect().cardRect, isCenteredHorizontally, isCenteredVertically),
         Positioned(
-            top: _offset.dy,
-            left: _offset.dx,
-            child: IgnorePointer(
-              ignoring: widget.layerItem.ignorePoint,
-              child: _DraggablePoint(
-                onLayerTapped: () {
-                  widget.onLayerTapped?.call(layerItem);
-                },
-                onDragStart: (d) {
-                  widget.onDragStart?.call(layerItem);
-                  startingFingerPositionFromObject = d;
-                },
-                onDragEnd: () {
-                  _handleDeleteAction(false);
-                  widget.onDragEnd?.call(layerItem);
-                },
-                onDrag: widget.layerItem.isDraggable && widget.isFocus
-                    ? (d, focalPoint) async {
-                        setState(() {
-                          _offset = Offset(_offset.dx + d.dx, _offset.dy + d.dy);
-                          isCenteredHorizontally =
-                              _checkIfCentered(_offset, _size, GlobalRect().cardRect.size.width, Axis.horizontal);
-                          isCenteredVertically =
-                              _checkIfCentered(_offset, _size, GlobalRect().cardRect.size.height, Axis.vertical);
-                        });
-
-                        currentFingerPosition = startingFingerPositionFromObject + _offset;
-                        _handleDeleteAction(true);
-                      }
-                    : null,
-                onScale: widget.layerItem.isScalable && widget.isFocus ? (s) => _handleScale(s) : null,
-                onRotate: widget.layerItem.isRotatable && widget.isFocus
-                    ? (a) => setState(() {
-                          _angle = a;
-                        })
-                    : null,
-                child: Transform.rotate(
-                  angle: _angle,
-                  child: buildChild(),
-                ),
+          top: offset.dy,
+          left: offset.dx,
+          child: IgnorePointer(
+            ignoring: widget.layerItem.ignorePoint,
+            child: _DraggablePoint(
+              onLayerTapped: () {
+                widget.onLayerTapped?.call(layerItem);
+              },
+              onDragStart: (d) {
+                widget.onDragStart?.call(layerItem);
+                startingFingerPositionFromObject = d;
+              },
+              onDragEnd: () {
+                _handleDeleteAction(false);
+                widget.onDragEnd?.call(layerItem);
+              },
+              onDrag: widget.layerItem.isDraggable && widget.isFocus
+                  ? (d, focalPoint) async {
+                      offset = Offset(offset.dx + d.dx, offset.dy + d.dy);
+                      isCenteredHorizontally =
+                          _checkIfCentered(offset, size, GlobalRect().cardRect.size.width, Axis.horizontal);
+                      isCenteredVertically =
+                          _checkIfCentered(offset, size, GlobalRect().cardRect.size.height, Axis.vertical);
+                      currentFingerPosition = startingFingerPositionFromObject + offset;
+                      _handleDeleteAction(true);
+                    }
+                  : null,
+              onScale: widget.layerItem.isScalable && widget.isFocus ? (s) => _handleScale(s) : null,
+              onRotate: widget.layerItem.isRotatable && widget.isFocus ? (a) => angle = a : null,
+              child: Transform.rotate(
+                angle: angle,
+                child: buildChild(),
               ),
-            )),
+            ),
+          ),
+        )
       ],
     );
-  }
-
-  List<Widget> _buildCenterLine(bool isCenteredHorizontally, bool isCenteredVertically) {
-    return [
-      Positioned(
-        top: GlobalRect().cardRect.size.height / 2,
-        left: 0,
-        right: 0,
-        child: Container(
-          height: 1,
-          color: isCenteredVertically ? Colors.red : Colors.transparent,
-        ),
-      ),
-      Positioned(
-        left: GlobalRect().cardRect.size.width / 2,
-        top: 0,
-        bottom: 0,
-        child: Container(
-          width: 1,
-          color: isCenteredHorizontally ? Colors.red : Colors.transparent,
-        ),
-      ),
-    ];
   }
 
   void _handleScale(double scale) {
@@ -157,17 +162,60 @@ class _DraggableResizableState extends State<DraggableResizable> {
       widget.layerItem.rect.size.height * scale,
     );
 
-    final midX = _offset.dx + (_size.width / 2);
-    final midY = _offset.dy + (_size.height / 2);
+    final midX = offset.dx + (size.width / 2);
+    final midY = offset.dy + (size.height / 2);
     final updatedPosition = Offset(
       midX - (updatedSize.width / 2),
       midY - (updatedSize.height / 2),
     );
 
-    setState(() {
-      _size = updatedSize;
-      _offset = updatedPosition;
-    });
+    size = updatedSize;
+    offset = updatedPosition;
+  }
+
+  Widget buildChild() {
+    try {
+      switch (widget.layerItem.type) {
+        case LayerType.text:
+          object as TextBoxInput;
+
+          return TextBox(
+            isReadOnly: true,
+            input: object,
+          );
+        case LayerType.backgroundColor:
+          return Container(
+            height: size.height,
+            width: size.width,
+            color: widget.layerItem.object as Color,
+          );
+        case LayerType.drawing:
+          return Image.memory(
+            widget.layerItem.object as Uint8List,
+            fit: BoxFit.fill,
+            width: size.width,
+            height: size.height,
+          );
+        case LayerType.backgroundImage:
+        case LayerType.frame:
+          return Image(
+            image: widget.layerItem.object as ImageProvider,
+            fit: BoxFit.fill,
+            width: size.width,
+            height: size.height,
+          );
+        case LayerType.selectImage:
+        case LayerType.sticker:
+        default:
+          return SizedBox(
+            height: size.height,
+            width: size.width,
+            child: widget.layerItem.object,
+          );
+      }
+    } catch (e) {
+      return const SizedBox.shrink();
+    }
   }
 
   bool _checkIfCentered(Offset position, Size size, double canvasDimen, Axis axis) {
@@ -176,45 +224,27 @@ class _DraggableResizableState extends State<DraggableResizable> {
     return (center - widgetCenter).abs() < 5;
   }
 
-  Widget buildChild() {
-    switch (widget.layerItem.type) {
-      case LayerType.text:
-        _object as TextBoxInput;
-
-        return TextBox(
-          isReadOnly: true,
-          input: _object,
-        );
-      case LayerType.backgroundColor:
-        return Container(
-          height: _size.height,
-          width: _size.width,
-          color: widget.layerItem.object as Color,
-        );
-      case LayerType.drawing:
-        return Image.memory(
-          widget.layerItem.object as Uint8List,
-          fit: BoxFit.fill,
-          width: _size.width,
-          height: _size.height,
-        );
-      case LayerType.backgroundImage:
-      case LayerType.frame:
-        return Image(
-          image: widget.layerItem.object as ImageProvider,
-          fit: BoxFit.fill,
-          width: _size.width,
-          height: _size.height,
-        );
-      case LayerType.selectImage:
-      case LayerType.sticker:
-      default:
-        return SizedBox(
-          height: _size.height,
-          width: _size.width,
-          child: widget.layerItem.object,
-        );
-    }
+  List<Widget> _buildCenterLine(Rect standard, bool isCenteredHorizontally, bool isCenteredVertically) {
+    return [
+      Positioned(
+        top: standard.size.height / 2,
+        left: 0,
+        right: 0,
+        child: Container(
+          height: 1,
+          color: isCenteredVertically ? Colors.red : Colors.transparent,
+        ),
+      ),
+      Positioned(
+        left: standard.size.width / 2,
+        top: 0,
+        bottom: 0,
+        child: Container(
+          width: 1,
+          color: isCenteredHorizontally ? Colors.red : Colors.transparent,
+        ),
+      ),
+    ];
   }
 }
 
